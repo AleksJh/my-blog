46636eff7f43c414dfc787358ab75daaffd95e46
AleksanJhangiryan <aleksanjhangiryan@gmail.com>
2024-11-09 20:10:47 +0400

    Added ability to email posts.

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,3 +129,9 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
